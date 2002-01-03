ee9dc0e29886bca49ea362237cc5ca57ae26e444
Alistair Riddoch <alriddoch@googlemail.com>
2002-01-03 14:55:33 +0000

    *** empty log message ***

---
+++ app/EntityLayer.cpp
@@ -0,0 +1,18 @@
+// This file may be redistributed and modified only under the terms of
+// the GNU General Public License (See COPYING for details).
+// Copyright (C) 2000-2001 Alistair Riddoch
+
+#include "EntityLayer.h"
+#include "GlView.h"
+
+#include <GL/gl.h>
+
+EntityLayer::EntityLayer(Model & model) : Layer(model, "background", "Entities")
+{
+}
+
+void EntityLayer::draw(GlView & view)
+{
+    float winsize = std::max(view.width(), view.height());
+
+}
---
+++ app/EntityLayer.h
@@ -0,0 +1,31 @@
+// This file may be redistributed and modified only under the terms of
+// the GNU General Public License (See COPYING for details).
+// Copyright (C) 2000-2001 Alistair Riddoch
+
+#ifndef EQUATOR_APP_ENTITYLAYER_H
+#define EQUATOR_APP_ENTITYLAYER_H
+
+#include "Layer.h"
+
+namespace Gtk {
+  class FileSelection;
+}
+
+class EntityLayer : public Layer {
+  private:
+    void importFile() { }
+    void load(Gtk::FileSelection *) { }
+    void cancel(Gtk::FileSelection *) { }
+  public:
+    explicit EntityLayer(Model &);
+    void draw(GlView & view);
+    void select(GlView & view, int x, int y) { }
+    void select(GlView & view, int x, int y, int w, int h) { }
+    void pushSelection() { }
+    void popSelection() { }
+    void dragStart(GlView & view, int x, int y) { }
+    void dragUpdate(GlView & view, float x, float y, float z) { }
+    void dragEnd(GlView & view, float x, float y, float z) { }
+};
+
+#endif // EQUATOR_APP_ENTITYLAYER_H
