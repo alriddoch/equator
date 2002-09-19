76334cefdd629c5b3d3cac15ebc40e5630c95926
Alistair Riddoch <alriddoch@googlemail.com>
2002-09-19 17:31:33 +0000

    2002-09-19  Al Riddoch  <alriddoch@zepler.org>
    	* app/Holo.cpp: Made holoworld renderer more efficient by using vertex arrays.

--- app/Holo.cpp
+++ app/Holo.cpp
@@ -27,24 +27,47 @@ void Holo::draw(GlView & view)
         yc = yc - yc % 10;
     }
 
-    glDepthMask(GL_FALSE);
+    // This needs to be divided into two sections, one which generates the vertex
+    // array, and the other which draws it, so we don't have to regenerate the
+    // array every time the screen is refreshed.
 
-    glBegin(GL_LINES);
+    // THere is an imprecision here. We need a less unweildy calculation for the
+    // number of lines we are going to draw
+    int numVertices = (numlines + 1) * 2 * 3 * 2 * 2;
+    float * varray = new float[numVertices];
+    int vCount = -1;
+
+    for (int i = -(numlines - numlines % incr); i <= numlines; i += incr) {
+        varray[++vCount] = xc - numlines;
+        varray[++vCount] = yc + i;
+        varray[++vCount] = 0.0f;
+        varray[++vCount] = xc + numlines;
+        varray[++vCount] = yc + i;
+        varray[++vCount] = 0.0f;
+
+        varray[++vCount] = xc + i;
+        varray[++vCount] = yc - numlines;
+        varray[++vCount] = 0.0f;
+        varray[++vCount] = xc + i;
+        varray[++vCount] = yc + numlines;
+        varray[++vCount] = 0.0f;
+    }
+
+    glDepthMask(GL_FALSE);
 
     glColor3f(0.0f, 0.3f, 0.0f);
 
-    for (int i = -(numlines - numlines % incr); i <= numlines; i += incr) {
-        // if (i == xc) continue;
-
-        glVertex3f(xc-numlines, yc+i, 0.0f);
-        glVertex3f(xc+numlines, yc+i, 0.0f);
-
-        glVertex3f(xc+i, yc-numlines, 0.0f);
-        glVertex3f(xc+i, yc+numlines, 0.0f);
-    }
+    // Draw the green grid.
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glVertexPointer(3, GL_FLOAT, 0, varray);
+    glDrawArrays(GL_LINES, 0, (vCount + 1) / 3);
+    glDisableClientState(GL_VERTEX_ARRAY);
 
     glColor3f(0.0f, 1.0f, 0.0f);
 
+    glBegin(GL_LINES);
+
+    // Draw the origin lines.
     glVertex3f(xc-numlines, 0.0f, 0.0f);
     glVertex3f(xc+numlines, 0.0f, 0.0f);
 
@@ -54,5 +77,7 @@ void Holo::draw(GlView & view)
     glEnd();
 
     glDepthMask(GL_TRUE);
+
+    delete varray;
 
 }
