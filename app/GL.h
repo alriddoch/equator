ad3595c3675ccd71e04506a9475666057569074e
Alistair Riddoch <alriddoch@googlemail.com>
2003-01-27 15:15:39 +0000

    *** empty log message ***

---
+++ app/GL.h
@@ -0,0 +1,18 @@
+// This file may be redistributed and modified only under the terms of
+// the GNU General Public License (See COPYING for details).
+// Copyright (C) 2000-2001 Alistair Riddoch
+
+#ifndef EQUATOR_APP_GL_H
+#define EQUATOR_APP_GL_H
+
+#define GL_GLEXT_PROTOTYPES
+#include <GL/gl.h>
+
+#ifndef GL_EXT_compiled_vertex_array
+extern PFNGLLOCKARRAYSEXTPROC glLockArraysExt;
+extern PFNGLUNLOCKARRAYSEXTPROC glUnlockArraysExt;
+#endif // GL_EXT_compiled_vertex_array
+
+extern bool have_GL_EXT_compiled_vertex_array;
+
+#endif
